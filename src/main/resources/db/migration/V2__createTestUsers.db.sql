79c37f11102ac2499d0396a275c4858b3bd054c8
Manfred (home) <Manfred@athome.org>
2014-10-26 02:21:11 +0200

    h2 console

--- src/main/resources/db/migration/V2__createTestUsers.db.sql
+++
@@ -1,6 +0,0 @@
--- test user creation
-
-insert into tt_user(ID, USER_NAME, DISPLAY_NAME, ACTIVE, VERSION) values(1, 'admin', 'predefined admin user', false, 1);
-insert into tt_user(ID, USER_NAME, DISPLAY_NAME, ACTIVE, VERSION) values(2, 'unit-test-user', 'unit-test user data.sql', true, 1);
-
-commit;
